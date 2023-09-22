faca85779cda2ced4b44f3273332a433ac2bb80b
Thomas Rolinger <tbrolin@cs.umd.edu>
2023-09-22 12:12:05 -0400

    Fixed error causing compiler to crash immediately
    Signed-off-by: Thomas Rolinger <tbrolin@cs.umd.edu>

--- frontend/include/chpl/uast/prim-ops-list.h
+++ frontend/include/chpl/uast/prim-ops-list.h
@@ -205,8 +205,8 @@ PRIMITIVE_R(MAYBE_AGGREGATE_ASSIGN, "may be aggregated assignment")
 
 // added by tbrolin
 PRIMITIVE_R(MAYBE_PREFETCH_CANDIDATE, "may be candidate for prefetching")
-PRIMITIVE_R(MAYBE_IE_CANDIDATE, "may be candidate for IE")
-PRIMITIVE_R(MAYBE_AGGR_CANDIDATE, "may be irregular aggregated write")
+PRIMITIVE_R(MAYBE_IE_CANDIDATE, "may be candidate for IE optimization")
+PRIMITIVE_R(MAYBE_AGGR_CANDIDATE, "may be candidate for aggregation optimization")
 
 PRIMITIVE_R(ERROR, "error")
 PRIMITIVE_R(WARNING, "warning")
